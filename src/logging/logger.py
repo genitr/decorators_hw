1244dae69f652eaa01ef25b9a27e71739d466088
Юрий Данильченко <genitra111@gmail.com>
2024-11-18 00:38:16 +1000

    Completed task 2

--- src/logging/logger.py
+++ src/logging/logger.py
@@ -15,7 +15,7 @@ def write_log_to_file(log_file_path: str, message: str) -> None:
         file.write(message + '\n')
 
 
-def logger(old_function):
+def logger1(old_function):
     """
     Декоратор, который записывает в файл детали выполнения функции.
 
@@ -39,3 +39,30 @@ def logger(old_function):
             raise
 
     return wrapper
+
+
+def logger2(path):
+    """
+    Усовершенствованная версия функции logger1(), в которой
+    добавлен параметр пути до файла логирования.
+
+    :param path: Путь к журналу логирования.
+    :return: Обернутую функцию с возможностью ведения журнала.
+    """
+    def decorator(old_function):
+        @wraps(old_function)
+        def wrapper(*args, **kwargs):
+            date = datetime.now().strftime('%d.%m.%y')
+            time = datetime.now().strftime('%H:%M')
+            try:
+                result = old_function(*args, **kwargs)
+
+                log_info = (f'Date: {date}, Time: {time}, Function: {old_function.__name__}, '
+                            f'args: {args}, kwargs: {kwargs}, Result: {result}')
+                write_log_to_file(path, log_info)
+                return result
+            except Exception as e:
+                print(f"Ошибка в функции {old_function.__name__}: {e}")
+                raise
+        return wrapper
+    return decorator
--- src/run.py
+++ src/run.py
@@ -2,7 +2,7 @@
 
 import os
 
-from src.logging.logger import logger
+from src.logging.logger import logger1, logger2
 
 
 def test_1():
@@ -10,15 +10,15 @@ def test_1():
     if os.path.exists(path):
         os.remove(path)
 
-    @logger
+    @logger1
     def hello_world():
         return 'Hello World'
 
-    @logger
+    @logger1
     def summator(a, b=0):
         return a + b
 
-    @logger
+    @logger1
     def div(a, b):
         return a / b
 
@@ -41,5 +41,47 @@ def test_1():
     for item in (4.3, 2.2, 6.5):
         assert str(item) in log_file_content, f'{item} должен быть записан в файл'
 
+
+def test_2():
+    paths = ('log_1.log', 'log_2.log', 'log_3.log')
+
+    for path in paths:
+        if os.path.exists(path):
+            os.remove(path)
+
+        @logger2(path)
+        def hello_world():
+            return 'Hello World'
+
+        @logger2(path)
+        def summator(a, b=0):
+            return a + b
+
+        @logger2(path)
+        def div(a, b):
+            return a / b
+
+        assert 'Hello World' == hello_world(), "Функция возвращает 'Hello World'"
+        result = summator(2, 2)
+        assert isinstance(result, int), 'Должно вернуться целое число'
+        assert result == 4, '2 + 2 = 4'
+        result = div(6, 2)
+        assert result == 3, '6 / 2 = 3'
+        summator(4.3, b=2.2)
+
+    for path in paths:
+
+        assert os.path.exists(path), f'файл {path} должен существовать'
+
+        with open(path) as log_file:
+            log_file_content = log_file.read()
+
+        assert 'summator' in log_file_content, 'должно записаться имя функции'
+
+        for item in (4.3, 2.2, 6.5):
+            assert str(item) in log_file_content, f'{item} должен быть записан в файл'
+
+
 if __name__ == '__main__':
     test_1()
+    test_2()
